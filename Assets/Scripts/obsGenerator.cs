8584eb61ebfbcc48dbedc6e2d86d846374b33ab8
Ruben Buelvas <rcbuelvasv@gmail.com>
2017-07-27 11:45:04 -0500

    New name: Ronbird / New icon too

--- Assets/Scripts/obsGenerator.cs
+++ Assets/Scripts/obsGenerator.cs
@@ -6,10 +6,12 @@ public class obsGenerator : MonoBehaviour
     public GameObject[] obs = new GameObject[5];
     public float secondsToGenerate = 1;
     System.Random rnd = new System.Random();
+    int c = 0;
 
     // Use this for initialization
     void Start ()
     {
+        c = 0;
         generate();
 	}
 	
@@ -21,29 +23,40 @@ public class obsGenerator : MonoBehaviour
 
     void generate()
     {
-        int points = pj.points;
+        
+      
         if (pj.isPlaying == true)
         {
-            if (points >= 40 - 1)
+            if (c > 40 - 1)
             {
+                c++;
                 Instantiate(obs[4], new Vector2(transform.position.x, ((float)(rnd.Next(-165, 165) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
             }
-            else if (points >= 30 - 1)
+            else if (c > 30 - 1)
             {
-                Instantiate(obs[3], new Vector2(transform.position.x, ((float)(rnd.Next(-130, 130) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
+                c++;
+                Instantiate(obs[2], new Vector2(transform.position.x, ((float)(rnd.Next(-130, 130) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
             }
-            else if (points >= 20 - 1)
+            else if (c > 20 - 1)
             {
-                Instantiate(obs[2], new Vector2(transform.position.x, ((float)(rnd.Next(-90, 90) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
+                c++;
+                Instantiate(obs[3], new Vector2(transform.position.x, ((float)(rnd.Next(-90, 90) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
             }
-            else if (points >= 10 - 1)
+            else if (c > 10 - 1)
             {
+                c++;
                 Instantiate(obs[1], new Vector2(transform.position.x, ((float)(rnd.Next(-120, 120) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
             }
             else
             {
+                c++;
                 Instantiate(obs[0], new Vector2(transform.position.x, ((float)(rnd.Next(-90, 90) / 100.0))), Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
                // Debug.Log(rnd.Next(-90, 90) / 1000.0);
+            }
+
+            if(c >= 50 - 1)
+            {
+                c = 20 - 1;
             }
         }
         Invoke("generate", secondsToGenerate);
