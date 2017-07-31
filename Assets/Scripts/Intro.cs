d15826901f3cfbf97ed9edd21ff2622dd359fbad
Ruben Buelvas <rcbuelvasv@gmail.com>
2017-07-30 23:46:36 -0500

    Intro text fixed

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -6,21 +6,22 @@ using UnityEngine.UI;
 public class Intro : MonoBehaviour {
 
     public Text loadingText;
-    public Text continueText;
+    //public Text continueText;
     // Use this for initialization
     void Start()
     {
-        loadingText.enabled = false;
+        //loadingText.enabled = false;
+        SceneManager.LoadScene("Game");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
             continueText.enabled = false;
             loadingText.enabled = true;
             SceneManager.LoadScene("Game");
-        }
+        }*/
     }
 }
